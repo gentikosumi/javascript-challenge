6a0d095803a34302d63e29ee0d7b6fc35ab8ba3f
Ghenti Kosumi <gentikosumi@gmail.com>
2020-04-02 19:41:33 -0400

    Adding UFO-level-2

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -19,16 +19,46 @@ function handleClick() {
   var button = d3.select("#filter-btn");
 
   var inputDate = d3.select("#datetime");
-
   var inputValue = inputDate.property("value");
-  console.log(typeof inputValue);
   console.log(data);
 
-  // filter
-  var filteredDate = data.filter(data => data.datetime === inputValue);
-  console.log(filteredDate);
+  var inputCity = d3.select("#city");
+  var CityValue = inputCity.property("value");
+  console.log(data)
 
-  buildTable(filteredDate);
+  // var inputState = d3.select("#state");
+  // var StateValue = inputState.property("value");
+
+
+  // filter
+  var filteredDate = data.filter(d => d.datetime === inputValue);
+  console.log(filteredDate);
+  var filteredCity = data.filter(c => c.city === CityValue);
+  console.log(filteredCity);
+  // var filteredState = data.filter(s => s.state === StateValue);
+  // console.log(filteredState);
+
+  var filteredData = data.filter((d => d.datetime === inputValue) && (c => c.city === CityValue));
+  console.log(filteredData);
+
+  let response = {
+    filteredData, filteredDate, filteredCity
+  }
+
+  if (response.filteredData.length !==0) {
+    buildTable(filteredData);
+  }
+    else if (response.filteredData.length === 0 && ((response.filteredDate.length !==0 || response.filteredDate.length !==0))) {
+      buildTable(filteredDate || buildTable(filteredCity));
+    }
+    
+    else {
+      tbody.append("tr").append("td").text("No results!");
+    }
+
+  
+  
+  // buildTable(filteredData);
 
 }
 
